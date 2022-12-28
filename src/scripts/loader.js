70cdc6f383797e92b49054e3415e686e3d074846
Alejo Dieguez <alejodieguez2012@gmail.com>
2022-12-28 00:53:33 -0300

    fix: formatted some code

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-    loader.classList.add("loader--hidden")
-    loader.addEventListener("transitionend", ()=> {
-        document.body.removeChild(loader)
-    })
+  const loader = document.querySelector(".loader");
+  loader.classList.add("loader--hidden");
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
 });
